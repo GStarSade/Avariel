4e8c42f34ec73c629f7dadbf777953c600ab0e14
Gideon Sassoon <gregister@kolsassoon.org.uk>
2017-01-21 15:10:00 +0000

    Implemented  • Fragment activity,  • ViewPager  • Skill sheet Fragment

---
+++ app/src/main/java/com/example/gideonsassoon/avariel/ui/MainFragmentActivity.java
@@ -0,0 +1,22 @@
+package com.example.gideonsassoon.avariel.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+import com.example.gideonsassoon.avariel.R;
+
+public class MainFragmentActivity extends FragmentActivity {
+    MainFragmentAdaptor mMainFragmentAdaptor;
+    ViewPager mViewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_fragment);
+        mMainFragmentAdaptor = new MainFragmentAdaptor(getSupportFragmentManager());
+        mViewPager = (ViewPager)findViewById(R.id.viewpager);
+        mViewPager.setAdapter(mMainFragmentAdaptor);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/gideonsassoon/avariel/ui/MainFragmentAdaptor.java
@@ -0,0 +1,48 @@
+package com.example.gideonsassoon.avariel.ui;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Gideon Sassoon on 21/01/2017.
+ */
+
+public class MainFragmentAdaptor extends FragmentPagerAdapter {
+
+    public MainFragmentAdaptor(FragmentManager fm) {
+        super(fm);
+    }
+
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position
+     */
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: {
+                return new SkillsSheetFragment();
+            }
+            case 1: {
+                return new SkillsSheetFragment(); //change skill sheet fragoment to attack sheet when done
+            }
+            case 2: {
+                break;
+            }
+            default: {
+                throw new RuntimeException("unhandled positing " + position);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
