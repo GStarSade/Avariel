ae60e7f094c6e31e60c3abfb050f2d719985687a
Gideon Sassoon <gregister@kolsassoon.org.uk>
2017-01-25 15:31:29 +0000

    Added icons for health and attributes. Changed strings to have separate entries for words needed colons.. Added current hp and total hp, including our first icon
    On another note, I nedd to deal with the warnings surrounding hints

--- app/src/main/java/com/example/gideonsassoon/avariel/ui/MainFragmentActivity.java
+++ app/src/main/java/com/example/gideonsassoon/avariel/ui/MainFragmentActivity.java
@@ -9,18 +9,13 @@ import com.example.gideonsassoon.avariel.R;
 public class MainFragmentActivity extends FragmentActivity {
     MainFragmentAdaptor mMainFragmentAdaptor;
     ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_fragment);
-
-
-
         mMainFragmentAdaptor = new MainFragmentAdaptor(getSupportFragmentManager());
-        mViewPager = (ViewPager)findViewById(R.id.viewpager);
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
         mViewPager.setAdapter(mMainFragmentAdaptor);
-
-
     }
 }
