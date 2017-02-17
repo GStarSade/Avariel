9e654088c6059df59f59566682e3d964b68a6d85
Gideon Sassoon <gregister@kolsassoon.org.uk>
2017-02-17 16:49:48 +0000

    Finished Abilities, Not certain if the layout will work on different screen sizes.
    Also Content description will need to be confirmed if done correctly. (Check with someone)
    
    Rotation also need to be sorted out

--- app/src/androidTest/java/com/example/gideonsassoon/avariel/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/gideonsassoon/avariel/ExampleInstrumentedTest.java
@@ -7,7 +7,7 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,11 +16,18 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+//    @Test
+//    public void useAppContext() throws Exception {
+//        // Context of the app under test.
+//        Context appContext = InstrumentationRegistry.getTargetContext();
+//
+//        assertEquals("com.example.gideonsassoon.avariel", appContext.getPackageName());
+//    }
+//
 
-        assertEquals("com.example.gideonsassoon.avariel", appContext.getPackageName());
+    @Test
+    public void tabTest() throws Exception {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        assertTrue(appContext.getClass().getName().equals("SkillsSheetFragment"));
     }
 }
---
+++ app/src/test/java/com/example/gideonsassoon/avariel/UnitTestWeapon.java
@@ -0,0 +1,39 @@
+package com.example.gideonsassoon.avariel;
+import com.example.gideonsassoon.avariel.data.Weapon;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by Gideon Sassoon on 11/02/2017.
+ */
+
+public class UnitTestWeapon {
+    Weapon axe;
+
+    @Before
+    public void setup() {
+        axe = new Weapon();
+        axe.setWeaponName("Battle Axe");
+        axe.setWeaponPropertiesAdditional("Versatile");
+    }
+    /**
+     * Weapon available.
+     * Are you armed?
+     * Cause it's dangerous to go alone!
+     */
+    @Test
+    public void weapon_available() throws Exception {
+        assertNotNull("You should be packing, but you're not!", axe);
+    }
+
+    @Test
+    public void weapon_properties() throws Exception {
+
+        assertEquals("Battle Axe",axe.getWeaponName());
+        assertEquals("Versatile", axe.getWeaponPropertiesAdditional());
+    }
+}
