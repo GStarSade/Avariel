041b1393fe250334308c20b20c490d437cd3ffc3
Gideon Sassoon <gregister@kolsassoon.org.uk>
2017-03-16 21:19:19 +0000

    Added Switch case for modifiers

--- app/src/main/java/com/example/gideonsassoon/avariel/data/Abilities.java
+++ app/src/main/java/com/example/gideonsassoon/avariel/data/Abilities.java
@@ -3,7 +3,6 @@ package com.example.gideonsassoon.avariel.data;
 /**
  * Created by Gideon Sassoon on 24/12/2016.
  */
-
 public class Abilities {
 
     int Strength;
@@ -59,5 +58,41 @@ public class Abilities {
 
     public void setCharisma(int charisma) {
         Charisma = charisma;
+    }
+
+    public int setModifer(String ability, int value) {
+        switch (value) {
+            case 1:
+                return -5;
+            case 2:
+            case 3:
+                return -4;
+            case 4:
+            case 5:
+                return -3;
+            case 6:
+            case 7:
+                return -2;
+            case 8:
+            case 9:
+                return -1;
+            case 10:
+            case 11:
+                return 0;
+            case 12:
+            case 13:
+                return 1;
+            case 14:
+            case 15:
+                return 2;
+            case 16:
+            case 17:
+                return 3;
+            case 18:
+            case 19:
+                return 4;
+            default:
+                return 0;
+        }
     }
 }
