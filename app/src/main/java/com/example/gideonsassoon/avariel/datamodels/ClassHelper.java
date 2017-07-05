0905c9759dffda2113cbd7ff556cc6aa40345f2b
scorchgid <gregister@kolsassoon.org.uk>
2017-07-05 18:08:51 +0100

    added helpers out lined return get methods for both class and race

--- app/src/main/java/com/example/gideonsassoon/avariel/datamodels/ClassHelper.java
+++ app/src/main/java/com/example/gideonsassoon/avariel/datamodels/ClassHelper.java
@@ -1,8 +1,82 @@
 package com.example.gideonsassoon.avariel.datamodels;
 
+import static com.example.gideonsassoon.avariel.datamodels.ClassHelper.Class.SORCERER;
+import static com.example.gideonsassoon.avariel.datamodels.ClassHelper.Class.WARLOCK;
+
 /**
  * Created by Gideon Sassoon on 13/05/2017.
  */
 
 public class ClassHelper {
+    public enum Class {
+        BARBARIAN, BARD, CLERIC, DRUID, FIGHTER, MONK, PALADIN, RANGER, ROGUE, SORCERER, WARLOCK, WIZARD;
+    }
+
+    public static ClassStats getPlayerClass(Class playerClass) {
+        switch (playerClass) {
+            case BARBARIAN:
+                return getBarbarian();
+            case BARD:
+                return getBard();
+            case CLERIC:
+                return getCleric();
+            case DRUID:
+                return getDruid();
+            case FIGHTER:
+                return getFighter();
+            case MONK:
+                return getMonk();
+            case PALADIN:
+                return getPaladin();
+            case RANGER:
+                return getRanger();
+            case ROGUE:
+                return getRogue();
+            case SORCERER:
+                return getSorcerer();
+            case WARLOCK:
+                return getWarlock();
+            case WIZARD:
+                return getWizard();
+            default:
+        }
+
+    }
+
+    static String getBarbarian() {
+
+    }
+
+    static void getBard() {
+    }
+
+    static void getDruid() {
+    }
+
+    static void getCleric() {
+    }
+
+    static void getFighter() {
+    }
+
+    static void getMonk() {
+    }
+
+    static void getPaladin() {
+    }
+
+    static void getRanger() {
+    }
+
+    static void getRogue() {
+    }
+
+    static void getSorcerer() {
+    }
+
+    static void getWarlock() {
+    }
+
+    static void getWizard() {
+    }
 }
--- app/src/main/java/com/example/gideonsassoon/avariel/datamodels/RaceHelper.java
+++ app/src/main/java/com/example/gideonsassoon/avariel/datamodels/RaceHelper.java
@@ -5,19 +5,39 @@ package com.example.gideonsassoon.avariel.datamodels;
  */
 
 public class RaceHelper {
-/*
+
     public enum Race {
-        HUMAN, MOUNTAIN_DWARF, HIGH_ELF
+        DRAGONBORN, HILL_DWARF, MOUNTAIN_DWARF, DROW_ELF, HIGH_ELF, WOOD_ELF, GNOME, HALF_ELF, HALF_ORC, HALFLING_LIGHTFOOT, HALFLING_STOUT, HUMAN, TIEFLING
     }
 
     public static RaceStats getRace(Race race) {
         switch (race) {
-            case HUMAN:
-                return getHuman();
+            case DRAGONBORN:
+                return getDragonborn();
+            case HILL_DWARF:
+                return getDwarf();
             case MOUNTAIN_DWARF:
                 return getMountianDwarf();
+            case DROW_ELF:
+                return getDrowElf();
             case HIGH_ELF:
                 return getHighElf();
+            case WOOD_ELF:
+                return getWoodElf();
+            case GNOME:
+                return getGnome();
+            case HALF_ELF:
+                return getHalfElf();
+            case HALF_ORC:
+                return getHalfOrc();
+            case HALFLING_LIGHTFOOT:
+                return getHalflingLightFoot();
+            case HALFLING_STOUT:
+                return getHalfingStout();
+            case HUMAN:
+                return getHuman();
+            case TIEFLING:
+                return getTiefling();
             default:
                 throw new RuntimeException("Unsupported Race :" + race);
         }
@@ -27,8 +47,6 @@ public class RaceHelper {
 
         //Abilities abilities = new Abilities(1, 1, 1, 1, 1, 1);
         //Race stats class stats and your player (What you rolled)
-
-        //Size
 
         //Speed
 
@@ -47,7 +65,7 @@ public class RaceHelper {
     }
 
     private static RaceStats getHighElf() {
-       // Abilities abilities = new Abilities(0, 2, 0, 0, 1, 0);
+        // Abilities abilities = new Abilities(0, 2, 0, 0, 1, 0);
         return new RaceStats(abilities);
-    }*/
+    }
 }
