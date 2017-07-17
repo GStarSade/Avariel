1bd3f6ab99961931ddda5fe057917e1b20d105dc
scorchgid <gregister@kolsassoon.org.uk>
2017-07-17 20:32:23 +0100

    Refactored sheet.getModifer to use formula over switch case (aka was "lazy")

--- app/src/main/java/com/example/gideonsassoon/avariel/datamodels/Sheet.java
+++ app/src/main/java/com/example/gideonsassoon/avariel/datamodels/Sheet.java
@@ -973,45 +973,7 @@ public class Sheet extends RealmObject {
     }
 
     private int setModifier(int value) {
-        switch (value) {
-            case 1:
-                return -5;
-            case 2:
-            case 3:
-                return -4;
-            case 4:
-            case 5:
-                return -3;
-            case 6:
-            case 7:
-                return -2;
-            case 8:
-            case 9:
-                return -1;
-            case 10:
-            case 11:
-                return 0;
-            case 12:
-            case 13:
-                return 1;
-            case 14:
-            case 15:
-                return 2;
-            case 16:
-            case 17:
-                return 3;
-            case 18:
-            case 19:
-                return 4;
-            case 20:
-            case 21:
-                return 5;
-            case 22:
-            case 23:
-                return 6;
-            default:
-                return 0;
-        }
+        return value / 2 - 5;
     }
 
     private int getCurrentLevel() {
