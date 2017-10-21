19af79bc2f36c68beddf4bbbb536b19d012ea38f
Gideon Sassoon <scorchgid@gmail.com>
2017-10-21 16:07:00 +0100

    Broken commiting before reversion Added buttons to java file, Extra TODOs Added onclick listeners for add row added and commented out text to measure buttons and confirm them to size Changed GridLayout to GridVIew

--- app/src/main/java/com/example/gideonsassoon/avariel/ui/CombatFragment.java
+++ app/src/main/java/com/example/gideonsassoon/avariel/ui/CombatFragment.java
@@ -3,12 +3,13 @@ package com.example.gideonsassoon.avariel.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.TextView;
 
 import com.example.gideonsassoon.avariel.R;
@@ -38,12 +39,20 @@ public class CombatFragment extends Fragment {
     TODO 3) define your attack item datamodel -  name, damage whatever.  This should be a realm object you can persist in your Sheet as entries in a RealmList
     TODO 4) use an ArrayAdapter in CombatFragment to populate the ListView with your attack entry items as the layout and the realmlist of attack items as the data source
     TODO 5) you'll need some other ui with which to create new attack entries - perhaps a dialog pop-up, or a "create new" button that creates an empty attack item model for you to edit and save.
+
+    TODO 6) try googling for "android list adapter tutorial" -
+    TODO 7) the idea with a list is not to manually add each entry to a layout with different content each time,
+
+    TODO 8) but to use an Adapter to populate a ListView with rows,
+    TODO 9) where the row is a separate layout that is inflated and populated from each entry in the data fed to the adapter.
+    TODO 10) You'll end up with your own ArrayAdapter extending class that knows how to turn your data into a row,
+    TODO 11) and you'll do listView.setAdapter(myArrayAdapter) to hook the two together.
+    TODO 12) There's a similar principle when using a GridView, if that's what you're after as well - Adapters are the key to turning a list of data into a list of views.
      */
 
     private Realm realm;
     Weapon weapon;
     Spell spell;
-
 
     @BindView(R.id.tv_armor_class_value)
     TextView tv_armorClassValue;
@@ -57,6 +66,12 @@ public class CombatFragment extends Fragment {
     TextView et_successValue;
     @BindView(R.id.et_failure_value)
     TextView et_failureValue;
+    @BindView(R.id.b_add_attack_spellcasting_row)
+    Button b_add_attack_spellcasting_row;
+    @BindView(R.id.rl_attack_spellcasting_content)
+    GridLayout rl_attack_spellcasting_content;
+    /*@BindView(R.id.b_delete_attack_spellcasting_row)
+    Button b_delete_attack_spellcasting_row;*/
 
     /*
     TODO Name - et_name_attack_spellcasting_value
@@ -66,7 +81,7 @@ public class CombatFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.content_combat, container, false);
         ButterKnife.bind(this, rootView);
         realm = Realm.getDefaultInstance();
@@ -113,6 +128,21 @@ public class CombatFragment extends Fragment {
             }
         });
 
+        /*
+        ATTaCK SPELLCASTING SECTION
+         */
+
+        b_add_attack_spellcasting_row.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Rel
+                //getLayoutInflater().inflate(R.layout.attack_view_list_item, rl_attack_spellcasting_content);
+
+            }
+        });
+
+
+        //buttonSetMatchingWidth();
         playerInit();
         return rootView;
     }
@@ -131,6 +161,17 @@ public class CombatFragment extends Fragment {
             }
         });
     }
+
+    /***
+     * https://stackoverflow.com/questions/9977721/how-to-set-width-which-is-equal-to-another-widget-on-android
+     */
+    /*private void buttonSetMatchingWidth() {
+        if (b_add_attack_spellcasting_row.getWidth() < b_delete_attack_spellcasting_row.getWidth())
+            b_add_attack_spellcasting_row.setWidth(b_delete_attack_spellcasting_row.getWidth());
+        else
+            b_delete_attack_spellcasting_row.setWidth(b_add_attack_spellcasting_row.getWidth());
+    }*/
+
     void addPlayerToUI(Sheet sheet) {
         tv_speedValue.setText(String.valueOf(sheet.getRaceSpeed()));
         et_failureValue.setText(String.valueOf(sheet.getFailureDeathSaves()));
